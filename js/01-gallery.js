405dac9f5d0c12a69e9e09970c57791b6a563adf
Kuzia30 <maxzimka37@gmail.com>
2021-10-18 10:50:47 +0300

    finish task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galeryWrap = document.querySelector('.gallery')
+const galeryWrap = document.querySelector(".gallery");
 
-galleryItems.forEach(createElement)
+galleryItems.forEach(createElement);
 
 function createElement(el) {
-    const link = document.createElement('a');
-    link.classList.add("gallery__link");
-    link.href = el.original;
-      
-    const image = document.createElement('img');
-    image.src = el.preview;
-    image.alt = el.description;
-    image.classList.add('gallery__image');
-    image.dataset.source = el.original;
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = el.original;
 
-    link.appendChild(image);
-    galeryWrap.appendChild(link)
+  const image = document.createElement("img");
+  image.src = el.preview;
+  image.alt = el.description;
+  image.classList.add("gallery__image");
+  image.dataset.source = el.original;
+
+  link.appendChild(image);
+  galeryWrap.appendChild(link);
 }
 
+galeryWrap.addEventListener("click", toggleInstanceImage);
+
+function toggleInstanceImage(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img
+      src=${event.target.dataset.source}
+    />
+    </div>`);
+
+  instance.show();
+
+  const modalEl = instance.element(".modal");
+  modalEl.addEventListener("click", onCloseInstanceImage);
+  document.addEventListener("keydown", onCloseInstanceImage);
+
+  function onCloseInstanceImage(evt) {
+    if (evt.target.nodeName === "IMG" || evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
