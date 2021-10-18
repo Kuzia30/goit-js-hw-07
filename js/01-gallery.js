13fccffe1f23d3f9362e95198e9888c72538ea0d
Kuzia30 <maxzimka37@gmail.com>
2021-10-18 06:34:03 +0300

    create img

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galeryWrap = document.querySelector('.gallery')
 
-console.log(galleryItems);
+galleryItems.forEach(createElement)
+
+function createElement(el) {
+    const link = document.createElement('a');
+    link.classList.add("gallery__link");
+    link.href = el.original;
+      
+    const image = document.createElement('img');
+    image.src = el.preview;
+    image.alt = el.description;
+    image.classList.add('gallery__image');
+    image.dataset.source = el.original;
+
+    link.appendChild(image);
+    galeryWrap.appendChild(link)
+}
+
